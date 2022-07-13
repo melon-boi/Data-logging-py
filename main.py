fe1406e8687d7a9eda97dc4bef165a4a8108c1f4
melon boi <melonboiwastaken@gmail.com>
2022-07-13 19:03:49 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello")
+import platform, socket, os, subprocess
+from requests import get
+
+os.system("cls")
+
+# Machine Info
+machine = platform.machine()
+platform = platform.platform()
+
+
+# Internet Info
+ip = get('https://api.ipify.org').text
+Internet_Citylocation = subprocess.check_output(f"curl ipinfo.io/{ip}/city", shell=False).decode('utf-8')
+Internet_Countrylocation = subprocess.check_output(f"curl ipinfo.io/{ip}/country", shell=False).decode('utf-8')
+
+
+# Print Data to Info
+
+print("Machine Information: \n")
+
+print(f"Machine: {machine}")
+print(f"Platform: {platform}")
+
+print("\nInternet Information: \n")
+
+print(f"IP: {ip}")
+print(f"Internet_CityLocation: {Internet_Citylocation}")
+print(f"Internet_CountryLocation: {Internet_Countrylocation}")
